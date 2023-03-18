429e5ae7ba07d096a245b799085c05e9bf0dd85a
Wesley <wesjchen@gmail.com>
2023-03-18 15:24:47 -0500

    update calculator class

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,34 +1,31 @@
 class Calculator
 
-    def initialize(numberOne, numberTwo)
-        @numberOne = numberOne
-        @numberTwo = numberTwo
+    def initialize(num1, num2)
+        @numberOne = num1
+        @numberTwo = num2
     end
 
-    def sum
-        return @numberOne + @numberTwo
-    end
+     def sum
+         return @numberOne + @numberTwo
+     end
 
-    def difference
-        return @numberOne - @numberTwo
-    end
-
-    def product
-        return @numberOne * @numberTwo
-    end
-
-    def quotient
-        if @numberTwo == 0
-            return 'not a valid number'
-        else
-           return @numberOne / @numberTwo 
-        end
-    end
-end
+     def difference
+         return @numberOne - @numberTwo
+     end
+     def product
+         return @numberOne * @numberTwo
+     end
+     def quotient
+         if @numberTwo == 0
+             return 'Error. Cannot divide by zero'
+         else
+            return @numberOne / @numberTwo 
+         end
+     end
+ end
 
 
 # Using the calculator
 
 a = Calculator.new(4,2)
-puts a.product
 puts a.quotient
